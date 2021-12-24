fa2e840413985de863db048953973095693ea9bc
bansal-shivam <56498037+bansal-shivam@users.noreply.github.com>
2021-12-24 18:10:37 +0530

    Add files via upload

---
+++ src/screens/DemoComponentReuse/ImageScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import ImageDetail from './components/ImageDetail';
+
+const ImageScreen = () => {
+  return (
+    <View>
+      <ImageDetail
+        title="Forest"
+        imageSource={require('../../../assets/forest.jpg')}
+      />
+      <ImageDetail
+        title="Beach"
+        imageSource={require('../../../assets/beach.jpg')}
+      />
+      <ImageDetail
+        title="Mountain"
+        imageSource={require('../../../assets/mountain.jpg')}
+      />
+    </View>
+  );
+};
+export default ImageScreen;
